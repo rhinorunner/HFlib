b29e5d252ffee881dc4673b2e481a7fd077a498a
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-03-24 12:34:15 -0400

    added HFL_version and a version history comment

--- HFlib/HFlib.hpp
+++ HFlib/HFlib.hpp
@@ -9,3 +9,17 @@
 #include "HF_math.hpp"
 #include "HF_basics.hpp"
 #include "HF_misc.hpp"
+
+static constexpr char* HFL_version = "1.0.1";
+
+/******************
+
+Version history:
+
+1.0.0 
+ 	- First release of HFlib!
+1.0.1
+ 	- HFlib version after docs were finished (probably not best to release the docs after the 1st release)
+ 	- Polished some things
+
+******************/
