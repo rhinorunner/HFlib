1f90091e3effe3bb4a03bcf64e866a1847dcb538
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-04-05 23:01:00 -0400

    Update HFlib.hpp

--- HFlib/HFlib.hpp
+++ HFlib/HFlib.hpp
@@ -2,10 +2,24 @@
 
 // Helpful Functions library (HFlib)
 // created by Zaine Rehman
-// from 12-7-22 to 3-23-23
+// from 12-7-22 to 4-5-23
+
+static constexpr char* HFL_version = "1.1.0";
 
 // hflib includes
 #include "HF_types.hpp"
 #include "HF_math.hpp"
 #include "HF_basics.hpp"
 #include "HF_misc.hpp"
+
+/******************
+Version history:
+1.0.0 
+ 	- First release of HFlib!
+1.0.1
+ 	- HFlib version after docs were finished (probably not best to release the docs after the 1st release)
+ 	- Polished some things
+1.1.0
+    - fixed/optimized a lot of the code
+    - added the Audio class to HF_basics
+******************/
